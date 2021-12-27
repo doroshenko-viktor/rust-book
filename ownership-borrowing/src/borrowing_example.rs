d3f33bdba35bac264121de05f0dbfeb5e90b0f0a
Viktor Doroshenko <reg.doroshenko@gmail.com>
2021-12-27 13:57:42 +0200

    refactor references

--- ownership-borrowing/src/borrowing_example.rs
+++ ownership-borrowing/src/borrowing_example.rs
@@ -51,3 +51,23 @@ fn test_multiple_mutable_references() {
 }
 
 // We also cannot have a mutable reference while we have an immutable one. Users of an immutable reference don’t expect the values to suddenly change out from under them! However, multiple immutable references are okay because no one who is just reading the data has the ability to affect anyone else’s reading of the data.
+// But it is possible to have multiple immutable references with mutable reference,
+// when they are live in different scopes. Scope counts from the place, where reference
+// is declared and until it's last usage
+fn multiple_mutable_and_immutable_references() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // variables r1 and r2 will not be used after this point
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
+    // println!("{} and {}", r1, r2); // this is not possible
+}
+
+#[test]
+fn test_multiple_mutable_and_immutable_references() {
+    multiple_mutable_and_immutable_references()
+}
