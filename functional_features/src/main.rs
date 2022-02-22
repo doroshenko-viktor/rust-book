7a41dc8663cae4f67048493e9f8729ee62fe85b7
Viktor Doroshenko <reg.doroshenko@gmail.com>
2022-02-22 19:55:48 +0200

    various cached results

--- functional_features/src/main.rs
+++ functional_features/src/main.rs
@@ -1,6 +1,6 @@
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
-//use std::time::Duration;
 
 fn main() {
     let simulated_user_specified_value = 10;
@@ -14,7 +14,7 @@ where
     T: Fn(u32) -> u32,
 {
     calculation: T,
-    value: Option<u32>,
+    values: HashMap<u32, u32>,
 }
 
 impl<T> Cacher<T>
@@ -24,19 +24,18 @@ where
     fn new(calculation: T) -> Self {
         Cacher {
             calculation,
-            value: None,
+            values: HashMap::new(),
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
+        if let Some(value) = self.values.get(&arg) {
+            return value.clone();
         }
+
+        let value = (self.calculation)(arg);
+        self.values.insert(arg, value);
+        value
     }
 }
 
