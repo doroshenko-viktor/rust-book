2d3bc81952dca3c7d1a2e832fd0a8d8c9fb377f0
Viktor Doroshenko <reg.doroshenko@gmail.com>
2022-02-23 21:53:21 +0200

    iterators

---
+++ iterators/src/lib.rs
@@ -0,0 +1,93 @@
+#[test]
+fn simple_iterator() {
+    let v1 = vec![1, 2, 3];
+    let v1_iter = v1.iter();
+
+    for val in v1_iter {
+        println!("Got: {}", val);
+    }
+}
+
+#[test]
+fn direct_iterator_call() {
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
+
+#[test]
+fn iterator_sum() {
+    let v1 = vec![1, 2, 3];
+
+    let v1_iter = v1.iter();
+
+    let total: i32 = v1_iter.sum();
+    // v1_iter is not available here, because `sum` took the ownership on it
+    assert_eq!(total, 6);
+}
+
+#[test]
+fn using_iterator_adapter() {
+    let v1: Vec<i32> = vec![1, 2, 3];
+    let v2 = v1.iter().map(|x| x + 1).collect::<Vec<i32>>();
+
+    assert_eq!(v2, vec![2, 3, 4]);
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes
+        .into_iter()
+        .filter(|s| s.size == shoe_size)
+        .map(|x| x)
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn filters_by_size() {
+        let shoes = vec![
+            Shoe {
+                size: 10,
+                style: String::from("sneaker"),
+            },
+            Shoe {
+                size: 13,
+                style: String::from("sandal"),
+            },
+            Shoe {
+                size: 10,
+                style: String::from("boot"),
+            },
+        ];
+
+        let in_my_size = shoes_in_size(shoes, 10);
+
+        assert_eq!(
+            in_my_size,
+            vec![
+                Shoe {
+                    size: 10,
+                    style: String::from("sneaker")
+                },
+                Shoe {
+                    size: 10,
+                    style: String::from("boot")
+                },
+            ]
+        );
+    }
+}
