17cb60d302c20a0c4a2df45b2b07d2bad4de6311
Viktor Doroshenko <reg.doroshenko@gmail.com>
2022-03-10 13:50:06 +0200

    state refactor

--- oop/src/blog.rs
+++ oop/src/blog.rs
@@ -1,94 +1,92 @@
-mod blog {
-    pub struct Post {
-        state: Option<Box<dyn State>>,
-        content: String,
-    }
+pub struct Post {
+    state: Option<Box<dyn State>>,
+    content: String,
+}
 
-    impl Post {
-        pub fn new() -> Self {
-            Post {
-                state: Some(Box::new(Draft {})),
-                content: String::new(),
-            }
-        }
-
-        pub fn add_text(&mut self, text: &str) {
-            self.content.push_str(text);
-        }
-
-        pub fn content(&self) -> &str {
-            self.state.as_ref().unwrap().content(self)
-        }
-
-        pub fn request_review(&mut self) {
-            // Option.take moves out value and leaves None in it's place
-            if let Some(s) = self.state.take() {
-                self.state = Some(s.request_review())
-            }
-        }
-
-        pub fn approve(&mut self) {
-            if let Some(state) = self.state.take() {
-                self.state = Some(state.approve())
-            }
+impl Post {
+    pub fn new() -> Self {
+        Post {
+            state: Some(Box::new(Draft {})),
+            content: String::new(),
         }
     }
 
-    pub trait State {
-        // Box<Self> means the method is only valid when called on a Box holding the type.
-        // This syntax takes ownership of Box<Self>, invalidating the old state so the state
-        // value of the Post can transform into a new state.
-        fn request_review(self: Box<Self>) -> Box<dyn State>;
-        fn approve(self: Box<Self>) -> Box<dyn State>;
-        fn content<'a>(&self, post: &'a Post) -> &'a str {
-            ""
+    pub fn add_text(&mut self, text: &str) {
+        self.content.push_str(text);
+    }
+
+    pub fn content(&self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn request_review(&mut self) {
+        // Option.take moves out value and leaves None in it's place
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review())
         }
     }
 
-    pub struct Draft {}
-
-    impl State for Draft {
-        fn request_review(self: Box<Self>) -> Box<dyn State> {
-            Box::new(PendingReview {})
-        }
-
-        fn approve(self: Box<Self>) -> Box<dyn State> {
-            self
+    pub fn approve(&mut self) {
+        if let Some(state) = self.state.take() {
+            self.state = Some(state.approve())
         }
     }
+}
 
-    pub struct PendingReview {}
+pub trait State {
+    // Box<Self> means the method is only valid when called on a Box holding the type.
+    // This syntax takes ownership of Box<Self>, invalidating the old state so the state
+    // value of the Post can transform into a new state.
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        ""
+    }
+}
 
-    impl State for PendingReview {
-        fn request_review(self: Box<Self>) -> Box<dyn State> {
-            self
-        }
+pub struct Draft {}
 
-        fn approve(self: Box<Self>) -> Box<dyn State> {
-            self
-        }
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
     }
 
-    pub struct Published {}
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
 
-    impl State for Published {
-        fn content<'a>(&self, post: &'a Post) -> &'a str {
-            &post.content()
-        }
+pub struct PendingReview {}
 
-        fn request_review(self: Box<Self>) -> Box<dyn State> {
-            self
-        }
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
 
-        fn approve(self: Box<Self>) -> Box<dyn State> {
-            self
-        }
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+pub struct Published {}
+
+impl State for Published {
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        &post.content()
+    }
+
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::blog::Post;
+    use super::Post;
 
     #[test]
     fn main_test() {
